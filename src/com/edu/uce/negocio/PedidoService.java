6ed94fd9bc823cd9a39f2b0f1f5fa53352f277e1
Joseph <danycondor851@gmail.com>
2026-04-13 19:49:24 -0500

    Tallersimple3

--- src/com/edu/uce/negocio/PedidoService.java
+++ src/com/edu/uce/negocio/PedidoService.java
@@ -10,16 +10,16 @@ public class PedidoService {
         // NotificadorMail n1 = new NotificadorMail();
         //n1.enviar(pedido.getCorreo(), "Su ha creado un pedido para ser atendido");
 
+        
+
         if (pedido.getTotal() > 100) {
-            // Enviar MAIL
             NotificadorMail n1 = new NotificadorMail();
             n1.enviar(pedido.getCorreo(), 
                       "Su pedido supera los $100 y será atendido prioritariamente");
         } else {
-            // Enviar SMS
             NotificacionSMS n2 = new NotificacionSMS();
             n2.enviar(pedido.getTelefono(), 
-                      "Su pedido ha sido registrado correctamente");
+                     "Su pedido ha sido registrado correctamente");
         }
     }
 }
